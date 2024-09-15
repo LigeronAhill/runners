99cb0fd54b8c845ebdfa2e7ebcbb2068184faa04
Aleksandr <provotorov@inbox.ru>
2024-09-15 17:53:16 +0300

    main func

--- cmd/runners/main.go
+++ cmd/runners/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	_ "github.com/lib/pq"
 	"log"
 	"runners/config"
+	"runners/server"
 )
 
 func main() {
+	log.Println("Starting Runners App")
+	log.Println("Initializing configuration")
 	config, err := config.Init("runners")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(config)
+	log.Println("Initializing databese")
+	dbHandler, err := server.InitDatabase(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Initializing HTTP server")
+	httpServer := server.InitHttp(config, dbHandler)
+	httpServer.Start()
 }
